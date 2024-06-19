596336a32accf8e2833e8b2bd90c85e7390d52bd
wanwiset25 <wanwiset25@gmail.com>
2024-06-19 18:43:40 +0400

    optimize style

--- src/config.ts
+++ src/config.ts
@@ -58,8 +58,21 @@ const environment = process.env.NODE_ENV || "production";
 export const devMode = environment != "production";
 
 const getZeroConfig = (): XdcZeroConfig => {
-  const isEnabled: boolean =  "PARENTNET_ZERO_CONTRACT" in process.env && "SUBNET_ZERO_CONTRACT" in process.env && "PARENTNET_ZERO_WALLET_PK" in process.env && "CHECKPOINT_CONTRACT" in process.env;
-  const isReverseEnabled: boolean =  "PARENTNET_ZERO_CONTRACT" in process.env && "SUBNET_ZERO_CONTRACT" in process.env && "SUBNET_ZERO_WALLET_PK" in process.env && "REVERSE_CHECKPOINT_CONTRACT" in process.env;
+  const reqXdcZero = [
+    "PARENTNET_ZERO_CONTRACT",
+    "SUBNET_ZERO_CONTRACT",
+    "PARENTNET_ZERO_WALLET_PK",
+    "CHECKPOINT_CONTRACT"
+  ];
+  const reqReverseXdcZero = [
+    "PARENTNET_ZERO_CONTRACT",
+    "SUBNET_ZERO_CONTRACT",
+    "SUBNET_ZERO_WALLET_PK",
+    "REVERSE_CHECKPOINT_CONTRACT"
+  ];
+  const isEnabled = reqXdcZero.every(envVar => envVar in process.env);
+  const isReverseEnabled = reqReverseXdcZero.every(envVar => envVar in process.env);
+
   let parentnetWalletPk = "";
   if (isEnabled){
     parentnetWalletPk = process.env.PARENTNET_ZERO_WALLET_PK.startsWith("0x") ? process.env.PARENTNET_ZERO_WALLET_PK : `0x${process.env.PARENTNET_ZERO_WALLET_PK}`;
